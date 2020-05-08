c12c393a413eabe8ce25b3af8cdd6df74660dc9e
Justin Schulberg <schulbergjustin@gmail.com>
2020-05-08 13:46:47 -0400

    Added linear regression

--- Code/Analysis/Modeling Code.R
+++ Code/Analysis/Modeling Code.R
@@ -60,6 +60,152 @@ summary(data_crime)
 # Crime		crime rate: number of offenses per 100,000 population in 1960
 
 
+########################################################################
+# Linear Regression ----------------------------------------------------
+######################################################################## 
+# In the first section, we'll use a simple linear regression model to
+# predict on our crime data.
+# Start by scaling the data so it's standardardized. Avoid column 2 because 
+# it's an indicator (factor) for southern states
+data_scaled <- as_tibble(scale(data_crime[, c(1, 3:16)]))
+
+# bring column 2 back in and reorder our columns
+data_scaled <- data_scaled %>%
+  cbind(data_crime[, 2]) %>%
+  select(Crime, everything())
+
+# Before we build our first Linear Regression model, let's set our seed to ensure randomness
+set.seed(123)
+
+# Let's also turn off scientific notation, because it'll make our results messy to read
+options(scipen = 999)
+
+### Build our first model using every variable as an output. Print the results
+(model_lm1 <- lm(Crime ~ ., data = data_scaled))
+
+summary(model_lm1)
+# The first two items we should look at are the R-squared value (80.3%) and the Adjusted R-squared value (71.8%)
+# The next item we should look at is the overal p-value, which is p-value: 0.0000003539. This means that our crime data
+# can be explained by the amalgam of predictor variables we provided
+
+
+
+# What do our coefficients look like?
+model_lm1$coefficients
+# As an example of what this means, if M (percentage of males aged 14-24 in total state population) increases by 1,
+# we would expect crime to increase by 87.83 crimes per 100,000 population.
+
+# But which of the variables mattered and which did not? Well, to start, let's look at the individual p-values for each
+# variable we have. We'll do this by pulling out the p-values and coefficient names.
+p_vals <- summary(model_lm1)$coefficients[,4]
+coefs <- rownames(summary(model_lm1)$coefficients)
+
+# bring our coefficient names and p-values into a data frame
+p_vals_df <- as_tibble(cbind(coefs, p_vals))
+
+# remove the first row which includes the intercept
+p_vals_df <- p_vals_df[-1, ]
+
+# Make sure the p-values are read in as numerics
+p_vals_df$p_vals <- as.double(p_vals_df$p_vals)
+
+# Save our visualization to the correct working directory
+setwd("C:/Users/jschulberg/Documents/Data Analytics Blog/Blog 3 - US Crime/us-crime-analysis/Viz/")
+jpeg(file = "U.S. Crime Linear Regression") # Name of the file for the viz we'll save
+
+# Visualization time
+ggplot(p_vals_df,
+       # order by size of the p-values
+       aes(x = reorder(coefs, -p_vals), y = p_vals)) +
+  # Let's make it a bar graph and change the color
+  geom_bar(stat = "identity", fill = "slateblue2") +
+  # Change the theme to minimal
+  theme_classic() +
+  # Let's change the names of the axes and title
+  xlab("Predictor Variable") +
+  ylab("P-Value*") +
+  labs(title = "P-Values for Different Predictors in the U.S. Crime Dataset",
+       subtitle = "*The dashed line shows which p-values are above or below the threshold of .05") +
+  # format our title and subtitle
+  theme(plot.title = element_text(hjust = 0, color = "black"),
+        plot.subtitle = element_text(color = "dark gray", size = 10)) +
+  # let's create a gray dashed, drop-line at .05 so we can see which variables are under our threshold
+  geom_hline(yintercept = .05, linetype = "dashed", color = "light gray", size = .75) +
+  # flip the axes and fix the axis
+  coord_flip(ylim = c(0, 1)) 
+
+dev.off()
+
+# Let's confirm what we see above, especially since U2 seems on the border. Filter our data frame
+# to only show p-values less than .05 and then order our results
+signif_vars <- p_vals_df %>%
+  filter(p_vals < 0.05) %>%
+  arrange(p_vals)
+
+# Pull the data back in
+data_signif <- data_scaled[which(colnames(data_scaled) %in% signif_vars$coefs)]
+
+# Bring Crime column back in
+data_signif <- cbind(data_signif, Crime = data_scaled$Crime)
+
+# Now that we have the coefficients that matter, let's re-run the linear model only on the coefficients we see above
+model_lm2 <- lm(Crime ~ ., data_signif)
+
+summary(model_lm2)
+# We see from this model that our R-squared value dropped significantly. Very strange, and not what I expected.
+# Let's try bringing in the two variables that were just above the .05 threshold (U@ and Po1).
+
+data_updated <- cbind(data_signif, U2 = data_scaled$U2, Po1 = data_scaled$Po1)
+
+model_lm3 <- lm(Crime ~., data_updated)
+
+summary(model_lm3)
+# Great! Our R-squared value went up to 76.6% and our adjusted R-squared went up to 73.1%. In particular, we notice
+# that our R-squared value has gone down (it was originally 80.3%); however, our adjusted R-squared value has gone up
+# (it was originally 70.1%). This makes sense because adjusted R-squared factors in the number of variables, and
+# should get closer to 1 the less we overfit our model.
+
+# Let's check out our coefficients with the new model
+model_lm3$coefficients
+
+# Now, let's predict on this fake dataset
+M = 14.0
+So = 0
+Ed = 10.0
+Po1 = 12.0
+Po2 = 15.5
+LF = 0.640
+M.F = 94.0
+Pop = 150
+NW = 1.1
+U1 = 0.120
+U2 = 3.6
+Wealth = 3200
+Ineq = 20.1
+Prob = 0.04
+Time = 39.0
+
+# Bring all this new data into one data frame, transposed so it's all in one row and counts as one observation
+new_data <- as.data.frame(t(c(M = M, So = So, Ed = Ed, Po1 = Po1, Po2 = Po2, 
+                              LF = LF, M.F = M.F, Pop = Pop, NW = NW, U1 = U1,
+                              U2 = U2, Wealth = Wealth, Ineq = Ineq, Prob = Prob, Time = Time)))
+
+
+# Predict what the crime rate would now be
+predict(model_lm3, new_data) 
+# The linear regression model estimates that the Crime rate based on this data will be 35.79. This translates to
+# about 36 crimes per 100,000 population. This seems extremely off from our original dataset, so we'll instead
+# resort to our original, unscaled data which included every variable.
+
+
+# Now let's take a look at the confidence interval for this new data
+predict(model_lm3, new_data, interval = "confidence")
+# The same linear regression model estimates 798 crimes per 100,000 population. It also estimates that 
+# the lower limit for our data is -886.6 and the upper limit is 2482.3. This result makes more sense based on the
+# fact that the median of our original crime is
+median(data_crime$Crime)
+
+
 
 #######################################################################
 # Principal Component Analysis ----------------------------------------
@@ -125,8 +271,7 @@ pca_cum_var <- pca_eig_val %>%
 # Clean up the dimensions so they're more interpretable and make sure they're read in as factors
 pca_cum_var$dimension <- as.factor(substr(pca_cum_var$dimension, 5, length(pca_cum_var$dimension)))
 
-# Save our visualization to the correct working directory
-setwd("C:/Users/jschulberg/Documents/Data Analytics Blog/Blog 3 - US Crime/us-crime-analysis/Viz/")
+
 jpeg(file = "U.S. Crime PCA Variation") # Name of the file for the viz we'll save
 
 # Viz time
@@ -155,7 +300,7 @@ ggplot(pca_cum_var, aes(x = dimension)) +
 
 
 ########################################################################
-# Linear Regression ----------------------------------------------------
+# Linear Regression w/ PCA ---------------------------------------------
 ######################################################################## 
 # Now we'll bring our crime data (our dependent variable) back in and see how our predictions are using the principle
 # components. First, let's limit our data to just the first six principle components, based on
@@ -168,18 +313,18 @@ pca_vals <- pca_vals %>%
   cbind(Crime = data_crime$Crime) %>%
   as_tibble()
 
-mod1 <- lm(Crime ~ ., data = pca_vals)
+model_pca <- lm(Crime ~ ., data = pca_vals)
 
-summary(mod1)
+summary(model_pca)
 # Notice that our adjusted R-squared value is 60.7%. This is significantly lower than what we saw in the previous homework (somewhere 
 # around 75-80%), but we've significantly reduced the number of variables we're using.
 
 ### Transform our data back
 # Get the original intercept from our model
-(b0 <- mod1$coefficients[1])
+(b0 <- model_pca$coefficients[1])
 
 # Pull out model coefficients so we can make the Beta vector
-(b_vals <- mod1$coefficients[2:(pca_len + 1)])
+(b_vals <- model_pca$coefficients[2:(pca_len + 1)])
 
 # To get our data re-transformed, we'll have to multiply the coefficients by our rotated matrix, A to create alpha vector
 # Note: The "%*%" operator enables us to perform matrix multiplication.
@@ -214,7 +359,8 @@ summary(estimates)
 # These are not exact, because our PCA model really only explains about 90% of the variation within our dataset.
 
 ## Predict
-# Now, let's predict on the dataset we've been provided. First we'll store all the new data we've been provided into a data frame.
+# Now, let's predict on this fake dataset below.
+# First we'll store all the new data we've been provided into a data frame.
 M = 14.0
 So = 0
 Ed = 10.0
